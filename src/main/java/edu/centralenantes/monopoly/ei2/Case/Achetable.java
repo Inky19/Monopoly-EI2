d30ab1795fb7504a14dc63ed59a5c4a91822339e
ManonC12 <manon-c.coutier@wanadoo.fr>
2022-11-17 11:55:09 +0100

    doc Achetable

--- src/main/java/edu/centralenantes/monopoly/ei2/Case/Achetable.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Achetable.java
@@ -10,71 +10,95 @@ import edu.centralenantes.monopoly.ei2.Joueur;
 import edu.centralenantes.monopoly.ei2.NoMoreMoney;
 
 /**
- *
- * @author inky19
+ * Classe abstraite assurant la gestion des Achetables
+ * @author Elouan
  */
 public abstract class Achetable extends Case {
-  private int prix;
-  private Joueur proprietaire;
+    
+    //ATTRIBUTS
+    /**
+     * prix d'achat de la case
+     */
+    private int prix;
+    /**
+     * propriétaire de la case
+     */
+    private Joueur proprietaire;
+    
+    //CONSTRUCTEURS
+    /**
+     * Crée un achetable paramétré
+     * @param num numéro de la case
+     * @param nom nom de la case
+     * @param prix prix d'achat de la case
+     * @param proprio propriétaire de la case
+     */
+    public Achetable(int num, String nom, int prix, Joueur proprio) {
+        super(num, nom);
+        this.prix = prix;
+        this.proprietaire = proprio;
+    }
+    
+    //GETTERS ET SETTERS
+    public int getPrix() {
+        return this.prix;
+    }
 
-  public Achetable(int num, String nom, int prix, Joueur proprio) {
-    super(num, nom);
-    this.prix = prix;
-    this.proprietaire = proprio;
-  }
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
 
-  public int getPrix() {
-    return this.prix;
-  }
+    public Joueur getProprietaire() {
+        return this.proprietaire;
+    }
 
-  public void setPrix(int prix) {
-    this.prix = prix;
-  }
-
-  public Joueur getProprietaire() {
-    return this.proprietaire;
-  }
-
-  public void setProprietaire(Joueur proprio) {
-    this.proprietaire = proprio;
-  }
-
-  public void acheter(Joueur j) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Vous possédez :" + j.getFortune() + "€");
-    System.out.println("Voulez-vous acheter :" + this.getNom() + " qui vaut " + this.prix + "€");
-    System.out.println("oui / non" );
-    String reponse = scan.nextLine();
-
-    boolean verif = true;
-    while(verif){
-      if (reponse.equals("oui")){
-        try {
-          j.payerBanque(this.prix);
-          this.setProprietaire(j);
-          if (this.getClass().getName().contains("Gare")) {
-            j.setNbGare(j.getNbGare() + 1);
-          }
-        } catch (NoMoreMoney exception) {
-          System.out.println("Vous n'avez pas assez d'argent pour acheter la case.");
-          System.out.println(" ");
-        }
-        verif = false;
-      }
-      else if (reponse.equals("non")){
-        verif = false;
-      }
-      else{
-        System.out.println("Erreur de saisie!");
+    public void setProprietaire(Joueur proprio) {
+        this.proprietaire = proprio;
+    }
+    
+    //AUTRES MÉTHODES
+    /**
+     * Permet à un joueur de devenir propriétaire d'un achetable
+     * @param j joueur
+     */
+    public void acheter(Joueur j) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Vous possédez :" + j.getFortune() + "€");
         System.out.println("Voulez-vous acheter :" + this.getNom() + " qui vaut " + this.prix + "€");
-        System.out.println("oui / non" );
-        reponse = scan.nextLine();
-      }
-    }
-    scan.close();
-  }
+        System.out.println("oui / non");
+        String reponse = scan.nextLine();
 
-  public abstract int loyer();
+        boolean verif = true;
+        while (verif) {
+            if (reponse.equals("oui")) {
+                try {
+                    j.payerBanque(this.prix);
+                    this.setProprietaire(j);
+                    if (this.getClass().getName().contains("Gare")) {
+                        j.setNbGare(j.getNbGare() + 1);
+                    }
+                } catch (NoMoreMoney exception) {
+                    System.out.println("Vous n'avez pas assez d'argent pour acheter la case.");
+                    System.out.println(" ");
+                }
+                verif = false;
+            } else if (reponse.equals("non")) {
+                verif = false;
+            } else {
+                System.out.println("Erreur de saisie!");
+                System.out.println("Vous possédez :" + j.getFortune() + "€");
+                System.out.println("Voulez-vous acheter :" + this.getNom() + " qui vaut " + this.prix + "€");
+                System.out.println("oui / non");
+                reponse = scan.nextLine();
+            }
+        }
+        scan.close();
+    }
+    
+    /**
+     * Calcule le loyer d'un achetable
+     * @return le loyer
+     */
+    public abstract int loyer();
 
 }
