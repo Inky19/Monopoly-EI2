950f26ad941ff5e59d5e99568e1c25261d426033
Domitilles <112643794+Domitilles@users.noreply.github.com>
2022-11-17 09:29:10 +0100

    Update Case.java

--- src/main/java/edu/centralenantes/monopoly/ei2/Case/Case.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Case.java
@@ -6,8 +6,56 @@ package edu.centralenantes.monopoly.ei2.Case;
 
 /**
  *
- * @author inky19
+ * @author Domitille
  */
-public class Case {
+public abstract class Case {
+    
+    // ATTRIBUTS
+
+    // numéro de la case entre 0 et 39 compris 
+    int numero;
+    
+    // nom de la case
+    String nom;
+    
+    // CONSTRUCTEUR
+    
+    /**
+     * Permet de créer une case à partir de son nom ainsi que de son numéro
+     * @param num = numéro
+     * @param name = nom
+     */
+    
+    public Case (int num, String name){
+        this.numero = num;
+        this.nom = name;
+    }
+    
+    // GETTERS & SETTERS
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * 
+     */
+    
+    @Override
+    public String toString() {
+        return "Case{" + "numero=" + numero + ", nom=" + nom + '}';
+    }
 
 }
