2950ecca9a389eb1188cbad9302750fe40bba8f4
Inky19 <f.marie260@gmail.com>
2022-11-17 08:59:45 +0100

    Ajout de la structure

---
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Pioche/Chance.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package edu.centralenantes.monopoly.ei2.Case.Pioche;
+
+/**
+ *
+ * @author inky19
+ */
+public class Chance {
+
+
+}
---
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Pioche/Communautaire.java
@@ -0,0 +1,11 @@
+
+package edu.centralenantes.monopoly.ei2.Case.Pioche;
+
+/**
+ *
+ * @author inky19
+ */
+public class Communautaire {
+
+
+}
