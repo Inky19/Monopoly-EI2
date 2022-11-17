ceb7a49ac1df9c6a1f6697c4576a0b5f19c82e56
Arthur200111 <arthur.huet01@gmail.com>
2022-11-17 09:22:48 +0100

    feat base de l'init du plateau

--- src/main/java/edu/centralenantes/monopoly/ei2/Plateau.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Plateau.java
@@ -4,11 +4,30 @@
  */
 package edu.centralenantes.monopoly.ei2;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.centralenantes.monopoly.ei2.Case.Case;
 /**
  *
  * @author inky19
  */
 public class Plateau {
+    List<Case> cases;
+    List<Joueur> joueurs;
 
+    public Plateau() {
+        this("/plateau/plateau.txt");
+    }
+
+    public Plateau (String path) {
+        cases = new LinkedList<>();
+        joueurs = new LinkedList<>();
+        initPlateau(path);
+    }
+
+    private void initPlateau(String path) {
+
+    }
 
 }
