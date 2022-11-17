8f10a5c11bbf873ca9093c48ce7ce0800ffd55f3
Domitilles <domitille.grandjean@eleves.ec-nantes.fr>
2022-11-17 12:01:38 +0100

    doc classe inutile

--- src/main/java/edu/centralenantes/monopoly/ei2/Case/Inutile.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Inutile.java
@@ -5,6 +5,11 @@ package edu.centralenantes.monopoly.ei2.Case;
  */
 public class Inutile extends Case{
     
+    /**
+     * Constructeur de la classe Inutile
+     * @param numero = numero de la carte
+     * @param nom = nom de la carte
+     */
     public Inutile(int numero, String nom){
         super(numero, nom);
     }
