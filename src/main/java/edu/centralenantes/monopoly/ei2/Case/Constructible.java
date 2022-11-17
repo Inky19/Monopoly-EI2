ac49cc5daed37c091c151dd8c607fcb3e8816d9e
ManonC12 <manon-c.coutier@wanadoo.fr>
2022-11-17 09:03:29 +0100

    Attributs Constructible

--- src/main/java/edu/centralenantes/monopoly/ei2/Case/Constructible.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Constructible.java
@@ -10,5 +10,22 @@ package edu.centralenantes.monopoly.ei2.Case;
  */
 public class Constructible {
 
-
+    //ATTRIBUTS
+    /**
+     * nombre de maison sur la case
+     */
+    private int nbMaisons;
+    /**
+     * nombre d'hôtels sur la case
+     */
+    private int nbHotels;
+    /**
+     * coefficient loyer des maisons
+     */
+    private int a;
+    /**
+     * coefficient loyer des hôtels
+     */
+    private int b;
+    
 }
