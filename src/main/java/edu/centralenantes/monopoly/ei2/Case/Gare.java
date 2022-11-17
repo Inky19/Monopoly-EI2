14a0ecd7b87d790eff3c6677a67ea9fcd2b9bcd6
Tristan <brardtristan@gmail.com>
2022-11-17 11:44:18 +0100

    Correction Gare

--- src/main/java/edu/centralenantes/monopoly/ei2/Case/Gare.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Gare.java
@@ -16,10 +16,6 @@ public class Gare extends Achetable {
         super(num, nom, prix, proprietaire);
     }
 
-    public Gare(int i, String libelle, int prix) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
     @Override
     public String toString() {
         String retour = this.getNom() + "(coût : " + this.getPrix() + " € ) - ";
