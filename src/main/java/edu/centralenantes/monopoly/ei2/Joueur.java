3cdd48021b99c5718be8da4899fb749dd3319901
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-17 10:24:55 +0100

    Ajout de getters et setters pour nbPrison, ajout d'un constructeur prenant en argument le nom et le plateau

--- src/main/java/edu/centralenantes/monopoly/ei2/Joueur.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Joueur.java
@@ -18,20 +18,30 @@ public class Joueur {
     boolean enPrison;
     int nbGare;
     Plateau plateau;
-
-    public Joueur(String nom, int fortune, int indexCase, boolean enPrison) {
+    public static final int FORTUNE_INITIALE = 100000;
+    public Joueur(String nom, int fortune, int indexCase, boolean enPrison, int nbGare, Plateau plateau) {
         this.nom = nom;
         this.fortune = fortune;
         this.indexCase = indexCase;
         this.enPrison = enPrison;
+        this.nbGare = nbGare;
+        this.plateau = plateau;
     }
-
+    
     public Joueur() {
         this.nom = "Bob";
-        this.fortune = 0;
+        this.fortune = 10000;
         this.indexCase = 0;
         this.enPrison = false;
+        this.nbGare = 0;
+        this.plateau = plateau;
     }
+    public Joueur(String nom, Plateau plateau){
+        this.nom = nom;
+        this.fortune = FORTUNE_INITIALE;
+        this.indexCase = 0;
+    }
+    
     public void payer(Joueur j, int payement) throws NoMoreMoney {
         if (this.fortune < payement) {
             throw new NoMoreMoney();
@@ -73,6 +83,16 @@ public class Joueur {
     public void setEnPrison(boolean enPrison) {
         this.enPrison = enPrison;
     }
+
+    public int getNbGare() {
+        return nbGare;
+    }
+
+    public void setNbGare(int nbGare) {
+        this.nbGare = nbGare;
+    }
+    
+    
     public ArrayList<Achetable> proprietes(){
         for(Case c : this.plateau.cases) {
             
