f7a9b664868724f6d4ccc1a50d7f92b723e2f940
ELBETTALO <eb.oussama3@gmail.com>
2022-11-17 09:52:41 +0100

    Classe Taxe

--- src/main/java/edu/centralenantes/monopoly/ei2/Case/Taxe.java
+++ src/main/java/edu/centralenantes/monopoly/ei2/Case/Taxe.java
@@ -5,10 +5,40 @@
 package edu.centralenantes.monopoly.ei2.Case;
 
 /**
- *
- * @author inky19
+ *Permet de créer/manipuler une case Taxe 
+ * @author Samar/ELBETTAL
  */
-public class Taxe {
+public class Taxe extends Case{
+     // prix de la taxe 
+     private int prix ;
+
+    /**
+     * Permet de créer une case Taxe à partir de son nom ainsi ,son numéro et son prix 
+     * @param num Numero de la case
+     * @param nom Nom de la case
+     * @param prix prix de la taxe
+     */
+    public Taxe(int num , String nom ,int prix) {
+        super(num , nom) ;
+        this.prix = prix;
+    }
+    // GETTERS & SETTERS
+    
+    /**
+     *
+     * @return prix de la taxe
+     */
+    public int getPrix() {
+        return prix;
+    }
+
+    /**
+     *
+     * @param prix prix de la taxe
+     */
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
 
 
 }
